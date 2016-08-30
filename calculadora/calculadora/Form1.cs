12af325a152b8ec7933f61c69ff817157f3488f1
delgadoguadamuz <delgadoguadamuz@hotmail.com>
2016-08-29 18:44:11 -0600

    cal form

---
+++ calculadora/calculadora/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculadora
+{
+    public partial class calculadora : Form
+    {
+        public calculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calculadora_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
